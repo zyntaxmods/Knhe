59476ba10eb6da680e5fb5373738df1d9f8ed85b
zyrill <zyntaxmods@gmail.com>
2024-11-06 07:10:22 +0800

    update

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ let button = document.getElementById("submit").addEventListener('click', functio
   let kenshie = crush.toLowerCase();
   if(kenshie == "kenshie" || kenshie == "jee" || kenshie == "kenshie jee rafinian" || kenshie == "red"){
     let happy = document.getElementById("nah").src = "happy.gif";
+    document.body.style.color = "green";
     setTimeout(function() {
       window.location.href = "confess.html";
     }, 3000);
   }
   else{
+    document.body.style.color = "red";
     let iyak = document.getElementById("nah").src = "pighati.gif";
   }
 })
